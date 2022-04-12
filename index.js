a862e83304be75f5a2fb4991e307f8b784afb6fc
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-12 12:43:34 +0300

    Change paragraph tag after content is loaded

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+////Test for mportance of DOMContentLoaded
+// document.addEventListener("DOMContentLoaded", function(event){
+    // console.log("Hello");
+   // const p=document.querySelector('p')
+    //console.log(p);
+// })
+// console.log("Before Load");
+// const p=document.querySelector('p')
+// console.log(p);
+//in head and beginning of the body, you have to load
+
+document.addEventListener("DOMContentLoaded", function(e){
+    document.querySelector("p").textContent = "This is really cool!" 
+})
+
